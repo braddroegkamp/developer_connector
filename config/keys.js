8b61c9ee7e43a8b6ede9082c341d38a42dfbc9ac
Brad and Aleshia Droegkamp <braddroegkamp@gmail.com>
2018-07-16 13:10:03 -0500

    updated config key logic and values

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb://brad.droegkamp:th1sisgreat@ds233571.mlab.com:33571/developer_connector",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV == "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
