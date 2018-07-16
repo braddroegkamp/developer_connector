f4bc9c772cb0990313e80748c1092e6bf0e055a0
Brad and Aleshia Droegkamp <braddroegkamp@gmail.com>
2018-07-16 12:01:17 -0500

    Added single post view with ability to add comments

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import {
   ADD_POST,
   GET_ERRORS,
+  GET_POST,
   GET_POSTS,
   POST_LOADING,
   DELETE_POST
@@ -45,6 +46,25 @@ export const getPosts = () => dispatch => {
     );
 };
 
+// Get Post
+export const getPost = id => dispatch => {
+  dispatch(setPostLoading());
+  axios
+    .get(`/api/posts/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POST,
+        payload: null
+      })
+    );
+};
+
 // Delete post
 export const deletePost = id => dispatch => {
   axios
@@ -81,6 +101,24 @@ export const removeLike = id => dispatch => {
   axios
     .post(`/api/posts/unlike/${id}`)
     .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Add Comment
+export const addComment = (postId, commentData) => dispatch => {
+  axios
+    .post(`/api/posts/comment/${postId}`, commentData)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
